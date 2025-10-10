22998951f9e16690539776d651063000a99e42e3
Ru2 <90049096+Rutujeet28@users.noreply.github.com>
2025-10-10 02:34:03 -0500

    Create 70. Climbing Stairs.py

---
+++ Python/70. Climbing Stairs.py
@@ -0,0 +1,19 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n == 1:
+            return 1
+        if n == 2:
+            return 2
+        
+        step1 = 1
+        step2 = 2
+        result  = 0
+
+        for i  in range(3,n+1):
+            result =  step1 + step2
+            step1 = step2
+            step2 = result
+            
+        return result
+
+        
